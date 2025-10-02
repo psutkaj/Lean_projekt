be65810a095ca014c477d968efa95c285dde49e7
Kuba185 <psutka.kuba@gmail.com>
2025-10-02 15:32:18 +0200

    a

--- LEANprj/Basic.lean
+++ LEANprj/Basic.lean
@@ -1,4 +1,6 @@
-def joinStringsWith (a : String) (b : String) (c : String) : String :=
-String.append b (String.append a c)
+import Mathlib
 
-joinStringsWith ", " "one" "and another"
+def joinStringsWith (a : String) (b : String) (c : String) : String :=
+  String.append b (String.append a c)
+
+#eval joinStringsWith ", " "one" "and another"
