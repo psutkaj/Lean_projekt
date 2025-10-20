e8a942a9be17346a4d54cc49d7e61952b02bcfb8
psutkaj <psutkaj@office365.zcu.cz>
2025-10-20 10:22:54 +0200

    s

--- LEANprj/Sequences/monotonic_sequences.lean
+++ LEANprj/Sequences/monotonic_sequences.lean
@@ -13,8 +13,6 @@ example (a : ℕ → ℝ) (ha: ∀ n : ℕ, a n = (n / (n + 1))) : StrictlyIncre
   rw [ha, ha]
   norm_cast
   simp
-  have h₁ : (↑n + 1) > 0 := by linarith
-  have h₂ : (↑n + 1 + 1) > 0 := by linarith
   refine (div_lt_div_iff₀ ?_ ?_).mpr ?_
   linarith
   linarith
