ad1366f0281f8806d84f59c91efdd9aee8c913fb
psutkaj <psutkaj@office365.zcu.cz>
2025-10-20 10:20:09 +0200

    s

--- LEANprj/Sequences/monotonic_sequences.lean
+++ LEANprj/Sequences/monotonic_sequences.lean
@@ -6,3 +6,16 @@ example (a : ℕ → ℝ) (ha: ∀ n : ℕ, a n = n) : StrictlyIncreasingSequenc
   rw [ha, ha]
   norm_cast
   linarith
+
+example (a : ℕ → ℝ) (ha: ∀ n : ℕ, a n = (n / (n + 1))) : StrictlyIncreasingSequence a := by
+  unfold StrictlyIncreasingSequence
+  intro n
+  rw [ha, ha]
+  norm_cast
+  simp
+  have h₁ : (↑n + 1) > 0 := by linarith
+  have h₂ : (↑n + 1 + 1) > 0 := by linarith
+  refine (div_lt_div_iff₀ ?_ ?_).mpr ?_
+  linarith
+  linarith
+  nlinarith
