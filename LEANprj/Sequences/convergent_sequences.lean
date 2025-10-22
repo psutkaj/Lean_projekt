c2efd3488c347f340f699cb355cec044ff59ec47
psutkaj <psutkaj@office365.zcu.cz>
2025-10-22 11:57:17 +0200

    conv

--- LEANprj/Sequences/convergent_sequences.lean
+++ LEANprj/Sequences/convergent_sequences.lean
@@ -1,7 +1,16 @@
 import LEANprj.Sequences.definitions
 
-example (a : ℕ → ℝ) (ha : ∀ n : ℕ, a n = (n / (n + 1))) : ConvergentTo a 1 := by
+example (a : ℕ → ℝ)  (ha :∀ n : ℕ, a n = 1 ) : ConvergentTo a 1 := by
   unfold ConvergentTo
   intro ε ε_pos
-  use (Int.ceil (1 / ε - 1)).toNat
-  sorry
+  use 0
+  intro n hn
+  rw [ha]
+  simp
+  linarith
+
+example (a : ℕ → ℝ) (ha : ∀ n : ℕ, a n = 1 / n) : ConvergentTo a 0 := by
+  intros ε ε_pos
+  use (Int.ceil (1/ε)).toNat
+  intro n n_gt
+  rw [ha]
