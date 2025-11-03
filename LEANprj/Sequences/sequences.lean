157ceb345b1205cfd3cfcfa0ad15573d37eb8583
psutkaj <psutkaj@office365.zcu.cz>
2025-11-03 18:41:29 +0100

    c

--- LEANprj/Sequences/sequences.lean
+++ LEANprj/Sequences/sequences.lean
@@ -8,4 +8,13 @@ noncomputable def Sup (A : Set ℝ) (hA : A.Nonempty) (hUpperBdd : ∃ u : ℝ, 
 noncomputable def Inf (A : Set ℝ) (hA : A.Nonempty) (hLowerBdd : ∃ l : ℝ, ∀ a ∈ A, l ≤ a) : ℝ :=
   choose (exists_unique_infimum A hA hLowerBdd)
 
-noncomputable def SupSeq (a : ℕ → ℝ) := Sup (Set.Range a)
+noncomputable def SupSeq (a : ℕ → ℝ) (hne : (Set.range a).Nonempty) (ha_upper_bdd : ∃ u : ℝ, ∀ c ∈ (Set.range a), c ≤ u) := Sup (Set.range a) hne ha_upper_bdd
+noncomputable def InfSeq (a : ℕ → ℝ) (hne : (Set.range a).Nonempty) (ha_lower_bdd : ∃ u : ℝ, ∀ c ∈ (Set.range a), c ≥ u) := Inf (Set.range a) hne ha_lower_bdd
+
+lemma Sup_isSup (A : Set ℝ) (hA : A.Nonempty) (hB : ∃ u : ℝ, ∀ a ∈ A, a ≤ u) : IsSup A (Sup A hA hB) := by
+  have hspec : IsSup A (choose (exists_unique_supremum A hA hB)) := (choose_spec (exists_unique_supremum A hA hB)).1
+  simpa
+
+lemma Inf_IsInf (A : Set ℝ) (hA : A.Nonempty) (hB : ∃ u : ℝ, ∀ a ∈ A, a ≥ u) : IsInf A (Inf A hA hB) := by
+  have hspec : IsInf A (choose (exists_unique_infimum A hA hB)) := (choose_spec (exists_unique_infimum A hA hB)).1
+  simpa
