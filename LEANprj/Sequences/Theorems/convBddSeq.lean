a24dbcf2c10aaa51491c9c6654b8a9426dec38ce
psutkaj <psutkaj@office365.zcu.cz>
2025-11-10 12:40:25 +0100

    convBddSeq

--- LEANprj/Sequences/Theorems/convBddSeq.lean
+++ LEANprj/Sequences/Theorems/convBddSeq.lean
@@ -23,27 +23,33 @@ theorem convImpBdd (a : ℕ → ℝ) (a_conv : Convergent a) : BoundedSequence a
   · have : 0 ≤ max K₁ (|q| + 1) := by exact le_max_of_le_right (by have := abs_nonneg q; linarith)
     have : 0 < max K₁ (|q| + 1) + 1 := by exact add_pos_of_nonneg_of_pos this (by norm_num)
     simpa
-  ·
-    sorry
+  · intro n
+    by_cases hn : n₀ ≤ n
+    · -- ukazem, ze vsechny prvky za n₀ jsou omezene
+      have htri : |a n| ≤ |a n - q| + |q| := by simpa using (abs_add (a n - q) q)
+      have h_lt_1 : |a n - q| < 1 := by exact hn₀ n hn
+      have : |a n| < 1 + |q| := by calc
+        |a n| ≤ |a n - q| + |q| := by exact htri
+        _ < 1 + |q| := by exact (add_lt_add_iff_right |q|).mpr (h_lt_1)
+      have : |q| + 1 < max K₁ (|q| + 1) + 1 := by
+        have : |q| + 1 ≤ max K₁ (|q| + 1) := by exact le_max_right _ _
+        exact lt_of_le_of_lt this (by linarith)
+      calc
+        |a n| < 1 + |q| := by (expose_names; exact this_1)
+        _ = |q| + 1 := by ring
+        _ < max K₁ (|q| + 1) + 1 := by linarith
+    · -- a ze i zbytek (tj. prvky pred n₀) je omezeny stejnou konstantou
+      have hnlt : n < n₀ := by linarith
+      have hmem : |a n| ∈ S := by
+        have : n ∈ Finset.range n₀ := Finset.mem_range.mpr hnlt
+        simpa [S] using Finset.mem_image.mpr ⟨n, this, rfl⟩
 
-
-  -- -- mez pro prvky posloupnosti > n₀
-  -- let K_n₀ := 1 + |q|
-  -- -- prvnich n₀ prvku posloupnosti v absolutni hodnote
-  -- let head_values := List.map (λ i => |a i|) (List.range n₀)
-  -- -- spojime prvni prvky s mezi pro zbytek
-  -- let all_values := head_values ++ [K_n₀]
-  -- -- vime, ze tento seznam prvku je neprazdny, protoze vzdy obsahuje alespon K_n₀
-  -- have h_nonempty : all_values ≠ [] := by exact List.concat_ne_nil K_n₀ head_values
-  -- -- vezmeme maximum z tohoto seznamu (fold nam vzdy pouzije max na jeden prvek a vrati ho, tj vezme prvni a porovna ho s nulou a brati vetsi, ktery pak porovna taky s nulou a vrati vetsi, takto najdu maximum konecneho seznamu)
-  -- let k := all_values.foldl max 0
-  -- let K := k + 1
-  -- use K
-  -- constructor
-  -- · have k_gt_Kn₀ : k ≥ K_n₀ := by
-
-
-  --     sorry
-  --   sorry
-  -- ·
-  --   sorry
+      have hS : S.Nonempty := ⟨|a n|, hmem⟩
+      have h_le_K₁ : |a n| ≤ K₁ := by
+        simp only [K₁, hS, dite_true]
+        exact Finset.le_max' S |a n| hmem
+      have K₁_le_K : K₁ < K := by
+        have : K₁ ≤ max K₁ (|q| + 1) := by exact le_max_left K₁ (|q| + 1)
+        have : K₁ < max K₁ (|q| + 1) + 1 := by exact lt_of_le_of_lt this (by linarith)
+        exact this
+      exact lt_of_le_of_lt h_le_K₁ K₁_le_K
