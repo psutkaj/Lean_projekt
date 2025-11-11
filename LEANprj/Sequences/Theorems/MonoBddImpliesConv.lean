8c9e286a187b5f77ae11db6de26e026702b46c40
psutkaj <psutkaj@office365.zcu.cz>
2025-11-11 15:00:26 +0100

    aa

--- LEANprj/Sequences/Theorems/MonoBddImpliesConv.lean
+++ LEANprj/Sequences/Theorems/MonoBddImpliesConv.lean
@@ -1,8 +1,29 @@
-import LEANprj.Sequences.Theorems.ConvImpliesBdd
+import LEANprj.Sequences.sequences
+open Classical
 
 theorem MonoBddImpliesConv (a : ℕ → ℝ) (ha_mono : MonotonicSequence a) (ha_bdd : BoundedSequence a) : Convergent a := by
   unfold Convergent
   unfold MonotonicSequence IncreasingSequence DecreasingSequence at ha_mono
   unfold BoundedSequence at ha_bdd
-  obtain ⟨s, hs⟩ := SupSeq a
-  sorry
+  obtain ⟨K, hK, K_bd⟩ := ha_bdd
+  let A : Set ℝ := Set.range a
+  -- rozdelime na dva pripady podle monotonie
+  rcases ha_mono with hinc | hdec
+  · let s : ℝ := SupSeq a (by use K; intro n; exact lt_of_abs_lt (K_bd n))
+    use s
+    unfold ConvergesTo
+    intro ε ε_pos
+    have hsup : IsSup A s := by
+      exact SupSeq_IsSup a (Exists.intro K fun n => lt_of_abs_lt (K_bd n))
+    have hexn : ∃ n, s - ε < a n := by
+      obtain ⟨_, hε⟩ := hsup (a 0) (by tauto)
+      obtain ⟨x, hxA, hxgt⟩ := hε ε ε_pos
+      obtain ⟨n, rfl⟩ := hxA
+      exact ⟨n, hxgt⟩
+    obtain ⟨N, hN⟩ := hexn
+    use N
+    intro m hm
+
+
+    sorry
+  · sorry
--- LEANprj/Sequences/sequences.lean
+++ LEANprj/Sequences/sequences.lean
@@ -3,18 +3,53 @@ import LEANprj.Sequences.Theorems.SupInfExistence
 open Classical
 -- v tomto souboru budoou dalsi definice, zavisle na nejakych vetach
 
+private def rangeNonempty (a : ℕ → ℝ) : (Set.range a).Nonempty := ⟨a 0, ⟨0, rfl⟩⟩
+
 noncomputable def Sup (A : Set ℝ) (hA : A.Nonempty) (hUpperBdd : ∃ u : ℝ, ∀ a ∈ A, a ≤ u) : ℝ :=
   choose (exists_unique_supremum A hA hUpperBdd)
 noncomputable def Inf (A : Set ℝ) (hA : A.Nonempty) (hLowerBdd : ∃ l : ℝ, ∀ a ∈ A, l ≤ a) : ℝ :=
   choose (exists_unique_infimum A hA hLowerBdd)
 
-noncomputable def SupSeq (a : ℕ → ℝ) (ha_upper_bdd : ∃ u : ℝ, ∀ c ∈ (Set.range a), c ≤ u) := Sup (Set.range a) (Set.range a).Nonempty ha_upper_bdd
-noncomputable def InfSeq (a : ℕ → ℝ) (hne : (Set.range a).Nonempty) (ha_lower_bdd : ∃ u : ℝ, ∀ c ∈ (Set.range a), c ≥ u) := Inf (Set.range a) hne ha_lower_bdd
+noncomputable def SupSeq (a : ℕ → ℝ) (ha_upper_bdd : UpperBoundedSequence a) := Sup (Set.range a) (rangeNonempty a) (by
+  obtain ⟨u, hu⟩ := ha_upper_bdd
+  use u
+  intro b hb
+  obtain ⟨n, hn⟩ := hb
+  rw [← hn]
+  exact le_of_lt (hu n)
+)
+noncomputable def InfSeq (a : ℕ → ℝ) (ha_lower_bdd : LowerBoundedSequence a) := Inf (Set.range a) (rangeNonempty a) (by
+  obtain ⟨l, hl⟩ := ha_lower_bdd
+  use l
+  intro b hb
+  obtain ⟨n, hn⟩ := hb
+  rw [←hn]
+  exact le_of_lt (hl n)
+)
+
 
 lemma Sup_isSup (A : Set ℝ) (hA : A.Nonempty) (hB : ∃ u : ℝ, ∀ a ∈ A, a ≤ u) : IsSup A (Sup A hA hB) := by
-  have hspec : IsSup A (choose (exists_unique_supremum A hA hB)) := (choose_spec (exists_unique_supremum A hA hB)).1
-  simpa
+  exact (choose_spec (exists_unique_supremum A hA hB)).1
 
 lemma Inf_IsInf (A : Set ℝ) (hA : A.Nonempty) (hB : ∃ u : ℝ, ∀ a ∈ A, a ≥ u) : IsInf A (Inf A hA hB) := by
-  have hspec : IsInf A (choose (exists_unique_infimum A hA hB)) := (choose_spec (exists_unique_infimum A hA hB)).1
-  simpa
+  exact (choose_spec (exists_unique_infimum A hA hB)).1
+
+lemma SupSeq_IsSup (a : ℕ → ℝ) (ha_upper_bdd : UpperBoundedSequence a) : IsSup (Set.range a) (SupSeq a ha_upper_bdd) := by
+  exact (choose_spec (exists_unique_supremum (Set.range a) (rangeNonempty a) (by
+  obtain ⟨u, hu⟩ := ha_upper_bdd
+  use u
+  intro b hb
+  obtain ⟨n, hn⟩ := hb
+  rw [← hn]
+  exact le_of_lt (hu n)
+))).1
+
+lemma InfSeq_IsInf (a : ℕ → ℝ) (ha_lower_bdd : LowerBoundedSequence a) : IsInf (Set.range a) (InfSeq a ha_lower_bdd) := by
+  exact (choose_spec (exists_unique_infimum (Set.range a) (rangeNonempty a) (by
+  obtain ⟨l, hl⟩ := ha_lower_bdd
+  use l
+  intro b hb
+  obtain ⟨n, hn⟩ := hb
+  rw [←hn]
+  exact le_of_lt (hl n)
+))).1
