78f70fbfe5fda3e7a9ce59bec51cef60e6ae617d
psutkaj <psutkaj@office365.zcu.cz>
2025-11-12 14:31:03 +0100

    a

--- LEANprj/Sequences/Theorems/BolzanoWeierstrass.lean
+++ LEANprj/Sequences/Theorems/BolzanoWeierstrass.lean
@@ -3,12 +3,10 @@ import LEANprj.Sequences.Theorems.ExMonoSubsequence
 
 theorem BolzanoWeierstrass (a : ℕ → ℝ) (ha_bdd : BoundedSequence a) : ∃ k : ℕ → ℕ, StrictlyIncreasingSequenceN k ∧ Convergent (Subsequence a k) := by
   obtain ⟨k, hk_inc, hk_mono⟩ := monoSubsequence a
-  use k
   have sub_bdd : BoundedSequence (Subsequence a k) := by
     obtain ⟨K, hK, hkn⟩ := ha_bdd
     refine ⟨K, hK, ?_⟩
     intro n
     exact hkn (k n)
-  constructor
-  · exact hk_inc
-  · exact MonoBddImpliesConv (Subsequence a k) hk_mono sub_bdd
+  refine ⟨k, hk_inc, ?_⟩
+  exact MonoBddImpliesConv (Subsequence a k) hk_mono sub_bdd
