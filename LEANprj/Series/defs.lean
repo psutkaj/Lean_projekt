a711d4b3bac2e2a6c0567910b635d61212be4ca9
psutkaj <psutkaj@office365.zcu.cz>
2025-11-19 12:13:36 +0100

    a

--- LEANprj/Series/defs.lean
+++ LEANprj/Series/defs.lean
@@ -5,6 +5,3 @@ import LEANprj.Sequences.defs
 def PartialSum (a : ℕ → ℝ) (n : ℕ) : ℝ := ∑ k ∈ Finset.range (n + 1), a k
 def SeriesConvergesTo (a : ℕ → ℝ) (s : ℝ) : Prop := ConvergesTo (PartialSum a) s
 def SeriesConvergent (a : ℕ → ℝ) : Prop := ∃ s : ℝ, SeriesConvergesTo a s
-
-
---#eval PartialSum (λ n ↦ n) 100
