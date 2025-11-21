e0e0b44a7c63f5764ea528cc5a273cf58c62a155
psutkaj <psutkaj@office365.zcu.cz>
2025-11-21 15:47:22 +0100

    d

--- LEANprj/_04Functions/defs.lean
+++ LEANprj/_04Functions/defs.lean
@@ -1,3 +1,3 @@
 import LEANprj._03Series.defs
 
-def CompactSet (M : Set ℝ) : Prop := ∀ a : ℕ → ℝ, (∀ n : ℕ, a n ∈ M) → ∃ k : ℕ → ℕ, StrictlyIncreasingSequenceN k ∧ Convergent (Subsequence a k)
+def CompactSet (M : Set ℝ) : Prop := ∀ a : ℕ → ℝ, (∀ n : ℕ, a n ∈ M) → ∃ k : ℕ → ℕ, StrictlyIncreasingSequenceN k ∧ ∃ l : ℝ, ConvergesTo (Subsequence a k) l ∧ l ∈ M
