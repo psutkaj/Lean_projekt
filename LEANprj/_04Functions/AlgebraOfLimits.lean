a4bd1fc5d5c865c7e1fbe8abf1ebdc0fbc1736ee
psutkaj <psutkaj@office365.zcu.cz>
2025-11-27 16:18:25 +0100

    alg

---
+++ LEANprj/_04Functions/AlgebraOfLimits.lean
@@ -0,0 +1,83 @@
+import LEANprj.defs
+
+theorem LimitAddFunction {f g : ℝ → ℝ} (x₀ b c : ℝ) (h₁ : CauchyLimitFunction f x₀ b) (h₂ : CauchyLimitFunction g x₀ c) : CauchyLimitFunction (f + g) x₀ (b + c) := by
+  unfold CauchyLimitFunction at *
+  intros ε₀ ε₀_pos
+  let ε := ε₀ / 2
+  have ε_pos : ε > 0 := by exact half_pos ε₀_pos
+  specialize h₁ ε ε_pos
+  specialize h₂ ε ε_pos
+  obtain ⟨δ₁, δ₁_pos, h₁⟩ := h₁
+  obtain ⟨δ₂, δ₂_pos, h₂⟩ := h₂
+  let δ := min δ₁ δ₂
+  have δ_pos : δ > 0 := by exact lt_min δ₁_pos δ₂_pos
+  refine ⟨δ, δ_pos, ?_⟩
+  intro x hx
+  simp
+  have hδ₁ : δ ≤ δ₁ := by exact min_le_left δ₁ δ₂
+  have hδ₂ : δ ≤ δ₂ := by exact min_le_right δ₁ δ₂
+  have : 0 < |x - x₀| ∧ |x - x₀| < δ₁ := by
+    constructor
+    · linarith
+    · calc
+        |x - x₀| < δ := hx.2
+        _ ≤ δ₁ := hδ₁
+  have ha := h₁ x this
+  have : 0 < |x - x₀| ∧ |x - x₀| < δ₂ := by
+    constructor
+    · linarith
+    · calc
+        |x - x₀| < δ := hx.2
+        _ ≤ δ₂ := hδ₂
+  have hb := h₂ x this
+  calc
+    |f x + g x - (b + c)| = |f x - b + (g x - c)| := by ring_nf
+    _ ≤ |f x - b| + |g x - c| := by apply abs_add _ _
+    _ < ε + ε := by exact add_lt_add ha hb
+    _ = ε₀ := by ring
+
+theorem LimitSubFunction {f g : ℝ → ℝ} (x₀ b c : ℝ) (h₁ : CauchyLimitFunction f x₀ b) (h₂ : CauchyLimitFunction g x₀ c) : CauchyLimitFunction (f - g) x₀ (b - c) := by
+  unfold CauchyLimitFunction at *
+  intros ε₀ ε₀_pos
+  let ε := ε₀ / 2
+  have ε_pos : ε > 0 := by exact half_pos ε₀_pos
+  specialize h₁ ε ε_pos
+  specialize h₂ ε ε_pos
+  obtain ⟨δ₁, δ₁_pos, h₁⟩ := h₁
+  obtain ⟨δ₂, δ₂_pos, h₂⟩ := h₂
+  let δ := min δ₁ δ₂
+  have δ_pos : δ > 0 := by exact lt_min δ₁_pos δ₂_pos
+  refine ⟨δ, δ_pos, ?_⟩
+  intro x hx
+  simp
+  have hδ₁ : δ ≤ δ₁ := by exact min_le_left δ₁ δ₂
+  have hδ₂ : δ ≤ δ₂ := by exact min_le_right δ₁ δ₂
+  have : 0 < |x - x₀| ∧ |x - x₀| < δ₁ := by
+    constructor
+    · linarith
+    · calc
+        |x - x₀| < δ := hx.2
+        _ ≤ δ₁ := hδ₁
+  have ha := h₁ x this
+  have : 0 < |x - x₀| ∧ |x - x₀| < δ₂ := by
+    constructor
+    · linarith
+    · calc
+        |x - x₀| < δ := hx.2
+        _ ≤ δ₂ := hδ₂
+  have hb := h₂ x this
+  calc
+    |f x - g x - (b - c)| = |f x - b + (-1) * (g x - c)| := by ring_nf
+    _ ≤ |f x - b| + |-1 * (g x - c)| := by apply abs_add _ _
+    _ = |f x - b| + |(g x - c)| := by simp; exact abs_sub_comm c (g x)
+    _ < ε + ε := by exact add_lt_add ha hb
+    _ = ε₀ := by ring
+
+
+
+theorem LimitSequenceMul (a b : ℕ → ℝ) (A B : ℝ) :
+  ConvergesTo a A → ConvergesTo b B → ConvergesTo (a * b) (A * B) := by
+  intros convA convB
+  unfold ConvergesTo at *
+  intro ε ε_pos
+  sorry
--- LEANprj/_04Functions/UniquenessLimitFunction.lean
+++ LEANprj/_04Functions/UniquenessLimitFunction.lean
@@ -1,4 +1,6 @@
 import LEANprj.defs
+import LEANprj._02Sequences.Theorems.UniquenessTheorem
+
 
 theorem UniquenessLimitFunction (f : ℝ → ℝ) (x₀ a b : ℝ) (h₁ : HeineLimitFunction f x₀ a) (h₂ : HeineLimitFunction f x₀ b) : a = b := by
   by_contra hne
@@ -18,8 +20,13 @@ theorem UniquenessLimitFunction (f : ℝ → ℝ) (x₀ a b : ℝ) (h₁ : Heine
     use n₀
     intro n hn
     rw [abs_of_pos, add_sub_cancel_left]
-    have : 1 / (n₀ + 1) ≥ 1 / (n + 1) := by gcongr;
-
-    sorry
-
-  sorry
+    have : 1 / (n₀ + 1 : ℝ) ≥ 1 / (n + 1 : ℝ) := by gcongr;
+    calc
+      1 / (↑n + 1) ≤ 1 / (↑n₀ + 1) := this
+      _ < ε := hn₀
+    simp
+    linarith
+  have h_seq_a : ConvergesTo (f ∘ s) a := by exact h₁ s h_ne_x₀ h_conv_s
+  have h_seq_b : ConvergesTo (f ∘ s) b := by exact h₂ s h_ne_x₀ h_conv_s
+  have : a = b := Uniqueness (f ∘ s) a b h_seq_a h_seq_b
+  contradiction
